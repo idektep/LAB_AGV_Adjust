3eedf5d7050b13f50a57352366d43efa99e96e41
idektep <idektep.coach@gmail.com>
2025-01-30 17:52:24 +0700

    Update AGV_Adjust.ino

--- AGV_Adjust/AGV_Adjust.ino
+++ AGV_Adjust/AGV_Adjust.ino
@@ -1,27 +1,27 @@
 //-------------------------------------------------Declare variable-------------------------------------------------------//
-#define MR_IN1 12 //motor1
-#define MR_IN2 13 //motor1
-#define MR_IN3 33 //motor2
-#define MR_IN4 32 //motor2
-#define ML_IN3 25 //motor3
-#define ML_IN4 26 //motor3
-#define ML_IN1 14 //motor4
-#define ML_IN2 27 //motor4
+#define MR_IN1 12 //backright motor
+#define MR_IN2 13 //backright motor
+#define MR_IN3 33 //frontright motor
+#define MR_IN4 32 //frontright motor
+#define ML_IN3 25 //backleft motor
+#define ML_IN4 26 //backleft motor
+#define ML_IN1 14 //frontleft motor
+#define ML_IN2 27 //frontleft motor
 
-#define L_ENA 15  //Pin speed motor1
-#define L_ENB 2   //Pin speed motor2
-#define R_ENA 19  //Pin speed motor4
-#define R_ENB 4   //Pin speed motor3
+#define BR_ENA 15  //Pin speed backright motor
+#define FR_ENB 2   //Pin speed frontright motor
+#define BL_ENB 4   //Pin speed backleft motor
+#define FL_ENA 19  //Pin speed frontleft motor
 
 #define L_LED 16  // Left LED
 #define R_LED 17  // Right LED
 
 //-----------------------------------------------Adjust range value 120-255-------------------------------------------------------//
 
-uint8_t SpeedM1 = 150;  //Adjust speed //motor1 
-uint8_t SpeedM2 = 150;  //Adjust speed //motor2
-uint8_t SpeedM3 = 150;  //Adjust speed //motor3
-uint8_t SpeedM4 = 150;  //Adjust speed //motor4
+uint8_t SpeedM1 = 150;  //Adjust speed //backright motor 
+uint8_t SpeedM2 = 150;  //Adjust speed //frontright motor
+uint8_t SpeedM3 = 150;  //Adjust speed //backleft motor
+uint8_t SpeedM4 = 150;  //Adjust speed //frontleft motor
 
 //-----------------------------------------------------------------------------------------------------------------------//
 
